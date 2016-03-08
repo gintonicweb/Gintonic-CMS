e3064a50915cefc49cdbb912c22163d71d0bb198
Philippe Lafrance <phil.laf@gmail.com>
2016-03-08 14:33:37 -0500

    Fallback routes should be handled by the app and not plugins.

--- config/routes.php
+++ config/routes.php
@@ -13,18 +13,14 @@ Router::scope('/', ['plugin' => 'GintonicCMS'], function ($routes) {
     $routes->connect('/posts', ['controller' => 'Posts']);
     $routes->connect('/posts/:action/*', ['controller' => 'Posts']);
 
-    $routes->fallbacks('DashedRoute');
-
     $routes->prefix('admin', function ($routes) {
 
         $routes->connect('/users', ['controller' => 'Users']);
         $routes->connect('/users/:action/*', ['controller' => 'Users']);
 
-
         $routes->connect('/posts', ['controller' => 'Posts']);
         $routes->connect('/posts/:action/*', ['controller' => 'Posts']);
 
-        $routes->fallbacks('DashedRoute');
     });
 
 });
